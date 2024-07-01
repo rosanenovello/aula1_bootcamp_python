95803a153ce0a3ce8d289e836acb0bc41d02b7a6
Rosane Novello <rosanenovello@gmail.com>
2024-07-01 19:11:34 -0300

    exemplo de print com input

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,18 @@
-print("Ola, " + input("Digite seu nome: ")+ "!")
+# print do primeiro input
+##print("Ola, " + input("Digite seu nome: ")+ "!")
+
+# retornar o numero de caracteres
+##print(len(input("Digite seu nome: ")))
+
+# digite 2 valores e paraeça a soma
+n1=0
+n2=0
+n1 = input("Digite o primeiro valor: ")
+n2 = input("Digite o segundo valor: ")
+print( (n1 + n2) )
+
+numero = 3 #int
+print(type(numero))
+#numero_decimal = 4.78 #float
+#nome_do_usuario = "rosane" #string
+#verdadeiro = True #bool
